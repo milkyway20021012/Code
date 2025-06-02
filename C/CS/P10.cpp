9bcf7925236c70f244592c5282c05892a5779974
milkyway20021012 <milkyway20021012@gmail.com>
2025-06-02 11:36:31 +0800

    0602Update

--- C/CS/P10.cpp
+++ C/CS/P10.cpp
@@ -9,19 +9,16 @@ public:
     }
     int to_decimal()
     {
-        int len = _str.size();
-        int num = stoi(_str);
-        int ret = 0;
-        int times = 1;
-        for (int i = 1; i <= len; ++i)
-        {
-            ret += (num % 10) * times;
-            times *= 2;
-            num /= 10;
-        }
-        return ret;
+        return translate(_str);
     }
     int to_decimal(string str)
+    {
+        return translate(str);
+    }
+
+private:
+    string _str;
+    int translate(const string &str)
     {
         int len = str.size();
         int num = stoi(str);
@@ -35,9 +32,6 @@ public:
         }
         return ret;
     }
-
-private:
-    string _str;
 };
 int main()
 {
